109caeb9a66478dbd75ab1534d7416fc5bf2cb11
Connor Day <connor.t.day@gmail.com>
2023-09-06 21:48:04 -0400

    Handle ready checks

--- back/src/game/lobby.ts
+++ back/src/game/lobby.ts
@@ -2,9 +2,12 @@ import { Room } from "./room";
 import { LobbySyncObject } from "../../../common/sync-objects";
 import { Player } from "./player";
 
+const START_DELAY = 5 * 1000;
+
 export class Lobby extends Room {
 
-    ready_status: { [key: string]: boolean } = {}
+    ready_status: { [key: string]: boolean } = {};
+    private _round_start_timeout?: NodeJS.Timeout;
 
     constructor(code: string) {
         super(code);
@@ -24,7 +27,41 @@ export class Lobby extends Room {
             console.log(`Player '${player.name}' has toggled ready to: '${this.ready_status[player.id]}'`);
 
             this.sync();
+
+            this.checkReady();
         });
+
+        this.checkReady();
+    }
+
+    removePlayer(player: Player, sync?: boolean): void {
+        super.removePlayer(player, sync);
+        this.checkReady();
+    }
+
+    checkReady() {
+        const all_ready = this.connected_players.every((player) => {
+            return this.ready_status[player.id];
+        });
+
+        if (all_ready) {
+            console.log(`All players in room '${this.code}' starting round timer`);
+
+            const startTime = Date.now() + START_DELAY;
+
+            this.emitAll("roundTimerStart", startTime);
+
+            this._round_start_timeout = setTimeout(() => {
+                console.log(`Room '${this.code}' has started a round`);
+                this.removeListeners();
+            }, START_DELAY);
+        } else if (this._round_start_timeout !== undefined) {
+            console.log("A player has become unready, stopping round timer");
+
+            clearTimeout(this._round_start_timeout);
+            this._round_start_timeout = undefined;
+            this.emitAll("roundTimerStop");
+        }
     }
 
     sync() {
